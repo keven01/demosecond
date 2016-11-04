8dba4a172e3e6143fc387f501b08bf80f2d933e6
keven01 <yangkai2018@163.com>
2016-11-04 16:48:46 +0800

    1

---
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -0,0 +1,36 @@
+package com.example;
+
+
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+//
+//
+//
+//public class DemoApplicationTests extends TestCase{
+//
+//	    /**
+//	     * Create the test case
+//	     *
+//	     * @param testName name of the test case
+//	     */
+//	    public DemoApplicationTests( String testName )
+//	    {
+//	        super( testName );
+//	    }
+//
+//	    /**
+//	     * @return the suite of tests being tested
+//	     */
+//	    public static TestSuite suite()
+//	    {
+//	        return new TestSuite( DemoApplicationTests.class );
+//	    }
+//
+//	    /**
+//	     * Rigourous Test :-)
+//	     */
+//	    public void testApp()
+//	    {
+//	        assertTrue( true );
+//	    }
+//	}
